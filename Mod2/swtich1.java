fded01849171294128e37df1c30bceadfea14fba
tharun <tharunsandu@gmail.com>
2026-08-05 12:24:07 +0530

    hii

---
+++ Mod2/swtich1.java
@@ -0,0 +1,21 @@
+public class swtich1 {
+    public static void main(String[] args) {
+        int a = 6;
+        switch (a) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Learning Days");
+                break;
+            case 6:
+                System.out.println("Revision Days");
+                break;
+            default:
+                System.out.println("Invalid ");
+        }
+
+    }
+}
+
