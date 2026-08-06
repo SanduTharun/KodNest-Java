4a94a3a8520b2189af76882601724c623b4a3707
tharun <tharunsandu@gmail.com>
2026-08-06 13:02:39 +0530

    Add Module 2 Java exercises and programs

---
+++ Mod2/continuee.java
@@ -0,0 +1,11 @@
+public class continuee {    
+    public static void main(String[] args) {
+        for(int i = 1 ; i<=10;i++){
+            if(i==6)
+            {
+                continue;
+            }
+            System.out.print(i);
+        }
+    }
+}
---
+++ Mod2/nested3.java
@@ -0,0 +1,13 @@
+public class nested3 {
+   public static void main(String[] args) {
+       outer : for(int i = 1; i <= 3; i++) 
+        {
+            inner : for(int j=1; j<=4; j++)  {
+               if(j == 2) {
+                   break outer;
+               }
+               System.out.println("i :" + i + " j: " + j);
+           }
+       } 
+   } 
+}
